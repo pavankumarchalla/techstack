8c862e568f0dc18fb7a45708f7ae2afe49aa0323
Pavan Kumar C <pavankumar@railsfactory.org>
2019-04-03 19:05:28 +0530

    common components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, SafeAreaView } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './reducers'
+import { Header } from './components/common'
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <SafeAreaView>
+        <View>
+          <Header title='Tech Stack' />
+        </View>
+      </SafeAreaView>
     </Provider>
   );
 }
